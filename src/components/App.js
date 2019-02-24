7298a39be3bbd4b223ece3a88edb0aa7292c96b6
VeeraVediyappan <veera.vediyappan@gmail.com>
2019-02-24 19:04:49 +0530

    Registraion page added

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,21 @@
 import React, { Component } from 'react';
 //import logo from './logo.svg';
-import '.././App.css';
-
+import '../../src/App.css';
+import Demo from './Registration'
 class App extends Component {
-  
+
   render() {
-    console.log(this.props,"THIS.PROPS");
+    console.log(this.props, "THIS.PROPS");
     return (
       <div className="App">
         <header className="App-header">
-        <button id='login-btn' onClick={ () => this.props.fnToggleLogin(this.props.loggedIn) } > {this.props.loggedIn ? 'Log Out' : 'Log In' }</button>
+          <button id='login-btn' onClick={() => this.props.fnToggleLogin(this.props.loggedIn)} > {this.props.loggedIn ? 'Log Out' : 'Log In'}</button>
+          
         </header>
+        <div>
+        {this.props.loggedIn ? 
+          <Demo /> : ''}
+        </div>
       </div>
     );
   }
---
+++ src/components/Registration.js
@@ -0,0 +1,113 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import '../../src/App.css';
+
+const styles = theme => ({
+  container: {
+
+    display: 'flex',
+    flexWrap: 'wrap',
+  },
+  textField: {
+    margin: '10px auto 10px auto;',
+    // marginLeft: theme.spacing.unit,
+    // marginRight: theme.spacing.unit,
+    width: '60%',
+  },
+});
+
+
+class Registration extends React.Component {
+  state = {
+    name: '',
+    email: '',
+    phone: '+91',
+  };
+
+  handleChange = name => event => {
+    this.setState({ [name]: event.target.value });
+  };
+
+  render() {
+    const { classes } = this.props;
+
+    return (
+
+      <div className="container">
+        <form className={classes.container} noValidate autoComplete="off">
+          <div className="header_register">Register with Email</div>
+          <TextField
+            id="standard-email"
+            label="Email"
+            className={classes.textField}
+            value={this.state.email}
+            onChange={this.handleChange('email')}
+            margin="normal"
+          />
+
+          <TextField
+            id="standard-name"
+            label="name"
+            value={this.state.name}
+            onChange={this.handleChange('name')}
+
+            className={classes.textField}
+            margin="normal"
+          />
+
+          <TextField
+            required
+            id="standard-phone"
+            label="phone"
+            value={this.state.phone}
+            onChange={this.handleChange('phone')}
+            className={classes.textField}
+            margin="normal"
+          />
+
+          <TextField
+            required
+            id="standard-country"
+            label="country"
+            value={this.state.country}
+            onChange={this.handleChange('country')}
+            className={classes.textField}
+            margin="normal"
+          />
+
+          <TextField
+            required
+            id="standard-state"
+            label="state"
+            value={this.state.state}
+            onChange={this.handleChange('state')}
+            className={classes.textField}
+            margin="normal"
+          />
+
+          <TextField
+            required
+            id="standard-city"
+            label="city"
+            value={this.state.city}
+            onChange={this.handleChange('city')}
+            className={classes.textField}
+            margin="normal"
+          />
+           
+        </form>
+        
+        </div>
+
+        );
+      }
+    }
+
+    Registration.propTypes = {
+          classes: PropTypes.object.isRequired,
+  };
+  
+  export default withStyles(styles)(Registration);
