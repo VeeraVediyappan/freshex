14e3614ffa741429e279665b07e7d13326e4e47e
VeeraVediyappan <veera.vediyappan@gmail.com>
2019-02-24 16:59:24 +0530

    test

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
@@ -17,7 +16,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Learn React test
           </a>
         </header>
       </div>
